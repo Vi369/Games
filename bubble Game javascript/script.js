c25218f3a191e60f35a332d347a71021c25c3b0b
vikas singh varma <118627416+Vi369@users.noreply.github.com>
2023-08-15 16:04:11 +0530

    Add files via upload

---
+++ bubble Game javascript/script.js
@@ -0,0 +1,63 @@
+let score = 0;
+let randomHit;
+
+function increaseScore(){
+    score +=10;
+    document.querySelector(".scoreValue").textContent = score ;  
+}
+//make bubble function
+
+function makeBubble(){
+    let clutter = "";
+
+for(let i =1; i<= 200; i++){
+    clutter += `<div class="bubble">${Math.floor(Math.random()*10)}</div>`;
+}
+
+document.querySelector("#pbtm").innerHTML = clutter;
+}
+//set timer function 
+
+function settimer(){
+    let timer = 12;
+    const stopTimer = setInterval(()=>{
+        if(timer>=0){
+            document.querySelector(".timerValue").textContent = timer;
+            timer--;   
+        }
+        else{
+            clearInterval(stopTimer);
+            document.querySelector("#pbtm").innerHTML = `<h1 id = "gameOver">Game Over </h1>
+            <button id="newGame">New Game</button>`;
+            document.querySelector("#newGame").addEventListener("click", startGame);
+            score = 0;
+        } 
+    }, 1000);
+    document.querySelector(".scoreValue").textContent = score ; 
+    
+}
+// new hit value function
+
+function getNewHit(){
+    randomHit = Math.floor(Math.random()*10);
+    document.querySelector(".hitValue").textContent = randomHit;
+}
+
+// start game fuction
+function startGame(){
+    makeBubble();
+    settimer();
+    getNewHit();
+}
+
+// calling functions
+startGame();
+
+document.querySelector("#pbtm").addEventListener("click", function(bubble){
+   const targetValue =  Number(bubble.target.textContent);
+   if(targetValue === randomHit){
+    increaseScore();
+    makeBubble();
+    getNewHit();
+   }
+});
